4e2b0024bf47a4f534dae4410fdef73e031cfdc1
Kasey Klipsch <kklipsch@mediamath.com>
2015-12-20 07:00:36 -0600

    add copyright header

--- pg/schema_reader.go
+++ pg/schema_reader.go
@@ -1,5 +1,9 @@
 package pg
 
+// Copyright 2015 MediaMath <http://www.mediamath.com>.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import (
 	"database/sql"
 	"fmt"
--- pg/wal/location.go
+++ pg/wal/location.go
@@ -1,5 +1,9 @@
 package wal
 
+// Copyright 2015 MediaMath <http://www.mediamath.com>.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import "fmt"
 
 // Location models a 64 bit address in the WAL
